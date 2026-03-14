f39bb9057c4ed1a17917c2288109f643831106ec
JaneBomb <jbombria@uccs.edu>
2026-03-14 01:32:49 +0000

    Created RoommatePost model for future use.

--- myproject/home/models.py
+++ myproject/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+def RoommatePost(models.Model):
+    '''
+    user = An instance from the Django built-in User class
+    date = Date of post, using date from datetime package (Format: date(YYYY, MM, DD))
+    message = Content of the roommate post
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    message = CharField(max_length = 200)
