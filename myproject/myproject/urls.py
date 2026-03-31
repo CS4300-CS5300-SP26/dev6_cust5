e939ba5014a51918267f367190d58528183ddd62
JaneBomb <jbombria@uccs.edu>
2026-03-31 21:48:58 +0000

    Updated work for interactive map. Made a new map HTML that can handle user input. Incorporated logcn into views.py for geocoding. Implemented feature on map, where user can input an address and map will create marker at address. Working on linking map to RentCast API.

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('roommate-posts/', include('home.urls')),
     path('register/', views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('map/', views.map_view, name='map'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
