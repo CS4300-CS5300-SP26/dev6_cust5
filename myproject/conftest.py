edcf7eaca44bbd614e3a418a62eacf9d18e81c3c
LochiRepo <lchinand@uccs.edu>
2026-04-20 02:18:52 -0600

    Merge Conflicts
    Fixed Merge Conflicts

--- myproject/conftest.py
+++ myproject/conftest.py
@@ -1,4 +1,10 @@
 import django
 import os
+from django.conf import settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 django.setup()
+
+#Use pytest to stop ASGI from breaking the runtime testing
+def pytest_configure():
+    settings.TESTING = True
